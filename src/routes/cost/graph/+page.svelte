fd240fd8c5533610935da6199abe1750f06cef1f
Arnav Shekaran <ashe292@aucklanduni.ac.nz>
2023-12-02 15:35:22 +0530

    graph for comparing costs of all grow runs

---
+++ src/routes/cost/graph/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Chart from 'chart.js/auto';
+	import { growRunsStore, resourcesList } from '$lib/grow-run/stores';
+	import 'chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm';
+
+	const randomMinDate = new Date('2020-01-01T00:00:00.000Z').valueOf();
+
+	$: data = {
+		labels: $growRunsStore.map((growRun) => growRun.name),
+		datasets: [
+			{
+				data: $growRunsStore.map((growRun) => ({
+					x: growRun.duration.start ? new Date(growRun.duration.start).valueOf() : randomMinDate,
+					y: growRun.calculateCostPer100g($resourcesList)
+				})),
+
+				backgroundColor: $growRunsStore.map((growRun) =>
+					growRun.duration.start ? 'powderblue' : '#124356'
+				)
+			}
+		]
+	};
+
+	let canvas: HTMLCanvasElement | undefined;
+	let scatterChart: Chart<'scatter', { x: number; y: number }[], string> | undefined;
+
+	$: if (canvas) {
+		if (scatterChart) scatterChart.destroy();
+		scatterChart = new Chart(canvas, {
+			type: 'scatter',
+			data,
+			options: {
+				scales: {
+					x: {
+						type: 'time',
+						min: randomMinDate
+					},
+					y: {
+						min: 0
+					}
+				}
+			}
+		});
+	}
+</script>
+
+<div>
+	<canvas bind:this={canvas} style="max-width: 90vw;max-height:90vh;"></canvas>
+</div>
