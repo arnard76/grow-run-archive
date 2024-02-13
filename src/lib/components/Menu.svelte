428cb972c367a8f7bbffa47014f1d482f3a5d6d5
Arnav Shekaran <ashe292@aucklanduni.ac.nz>
2024-02-13 17:45:27 +0530

    putting pies under cost tab

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -7,9 +7,9 @@
 		<p slot="text">Cost</p>
 		<a href="/cost/archive">Archive</a>
 		<a href="/cost/graph">Graph</a>
+		<a href="/cost/pie-charts">Pies 🥧📊</a>
 	</MenuDropDown>
 	<a href="/resources">Resources list</a>
-	<a href="/pie-charts">Pies 🥧📊</a>
 	<a href="/logout">Logout</a>
 </nav>
 
