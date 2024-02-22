b6c5a6772f7c08b91c6d9232e18251a601b1911e
Arnav Shekaran <ashe292@aucklanduni.ac.nz>
2024-02-22 16:58:24 +0530

    archiving product pages for resources
    when product link edited or new product is added
    
    all of it done on the frontend (no server API involved) although that ability is in sveltekit :)))

--- src/lib/resource/store.ts
+++ src/lib/resource/store.ts
@@ -3,6 +3,13 @@ import Resource from '$lib/resource';
 import { getDatabase, push, ref, set, onValue } from 'firebase/database';
 import { app } from '$lib/database/firebase';
 import { session } from '$lib/firebase/user';
+import {
+	getStorage,
+	uploadBytes,
+	ref as storageRef,
+	getDownloadURL,
+	deleteObject
+} from 'firebase/storage';
 
 const db = getDatabase(app);
 
@@ -64,6 +71,53 @@ export const resourcesList = {
 		return dbObject;
 	},
 
+	async archiveProductPageForResource({ productLink, name }: Resource) {
+		if (!productLink) throw Error('The url for the product page to be archived is missing.');
+
+		const queryParams = new URLSearchParams({
+			access_key: 'EPiZSx745qtheA',
+			url: productLink,
+			viewport_width: '1920',
+			viewport_height: '1280',
+			device_scale_factor: '1',
+			image_quality: '80',
+			format: 'jpg',
+			block_ads: 'true',
+			block_cookie_banners: 'true',
+			full_page: 'true',
+			block_trackers: 'true',
+			block_banners_by_heuristics: 'false',
+			delay: '0',
+			timeout: '60'
+		});
+
+		const screenshotApiResponse = await fetch('https://api.screenshotone.com/take?' + queryParams);
+
+		if (screenshotApiResponse.status != 200)
+			throw Error(
+				"couldn't screenshot product page: " +
+					productLink +
+					'\nbecause: ' +
+					(await screenshotApiResponse.text())
+			);
+
+		const arrayBuffer = await screenshotApiResponse.arrayBuffer();
+
+		// save screenshot to files to firebase storage
+		const screenshotPathRef = storageRef(
+			getStorage(),
+			`${get(session)?.user?.uid}/product-page-archive/${name}.jpg`
+		);
+
+		try {
+			await getDownloadURL(screenshotPathRef);
+			await deleteObject(screenshotPathRef);
+		} catch (e) {
+		} finally {
+			await uploadBytes(screenshotPathRef, arrayBuffer);
+		}
+	},
+
 	async addResource(resource: Resource) {
 		const { user } = get(session);
 		if (!user?.uid) return;
@@ -72,6 +126,7 @@ export const resourcesList = {
 
 		const newResourceRef = push(resourcesListRef);
 		await set(newResourceRef, resource);
+		this.archiveProductPageForResource(resource);
 	},
 
 	// local array is always up to date with db so can get directly from here
@@ -86,6 +141,10 @@ export const resourcesList = {
 	async editResource(resourceToEdit: Resource) {
 		const resources = get(this);
 		const index = resources.findIndex((resource) => resource.id === resourceToEdit.id);
+
+		if (resources[index].productLink != resourceToEdit.productLink)
+			this.archiveProductPageForResource(resourceToEdit);
+
 		resources[index] = resourceToEdit;
 		await this.updateResourcesListOnDb(resources);
 	},
