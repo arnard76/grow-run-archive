8d2ba30998a5cbf1bf9f790bc47dfe3a5116ab6f
Arnav <arnard76@gmail.com>
2025-03-06 22:27:09 +1300

    fix pipeline?

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('template spec', () => {
 	it('passes', () => {
-		cy.visit('http://localhost:5173/login');
+		cy.visit('/login');
 		cy.findByRole('button', { name: /submit/i });
 	});
 });
