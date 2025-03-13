84b55a22774f466963d0d0150968bdef5eb30150
Arnav <arnard76@gmail.com>
2025-03-14 10:06:11 +1300

    simpler

--- app/src/lib/components/Menu.svelte
+++ app/src/lib/components/Menu.svelte
@@ -4,10 +4,10 @@
 
 <nav class="flex gap-[15px] items-start">
 	<a href="/grow-runs">Grow Runs</a>
+	<a href="/resources">Resources</a>
 	<MenuDropDown>
 		<p slot="text">Cost</p>
 		<a href="/cost/graph">Graph</a>
 		<a href="/cost/pie-charts">Pies 🥧📊</a>
 	</MenuDropDown>
-	<a href="/resources">Resources list</a>
 </nav>
