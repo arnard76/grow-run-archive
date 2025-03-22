10972116c47213982aaa716320a2660099fc9084
Arnav <arnard76@gmail.com>
2025-03-22 20:43:15 +1300

    only set the https deploy server locally so that vercel can do its https magic in production

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import fs from 'fs';
+console.log(process.env);
+
+let configOptions =
+	process.env.NODE_ENV === 'development'
+		? {
+				server: {
+					https: {
+						key: fs.readFileSync(`${__dirname}/cert/key.pem`),
+						cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
+					}
+				}
+			}
+		: {};
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		https: {
-			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
-		}
-	}
+	...configOptions
 });
