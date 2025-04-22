dc766a9efe49476118b19d3ba159da549505f9e4
Arnav <arnard76@gmail.com>
2025-04-22 20:50:23 +1200

    update definitions

--- packages/definitions/src/condition.ts
+++ packages/definitions/src/condition.ts
@@ -1,7 +1,39 @@
-export type ConditionMeasurement = { dateTime: string; value: number };
+export type ConditionMeasurement = {
+	dateTime: ExternalConditionsMeasurements['dateTime'];
+	value: ExternalConditionsMeasurements['conditions'][''];
+};
 
 export type ConditionMeasurements = { [id: string | number]: ConditionMeasurement };
 
 export type ConditionsMeasurements = {
 	[conditionName in string]: ConditionMeasurements;
 };
+
+export type ExternalConditionsMeasurements = {
+	dateTime: string;
+	conditions: {
+		[conditionName in string]: number;
+	};
+};
+
+export function getConditionMetadata(conditionName?: keyof ConditionsMeasurements) {
+	if (conditionName && conditionsMetadata[conditionName]) return conditionsMetadata[conditionName];
+
+	return { units: 'NULLUNITS' };
+}
+
+type ConditionMetadata = { [key in string]: { units: string; verbose?: string } };
+
+export const conditionsMetadata: ConditionMetadata = {
+	humidity: { units: '%' },
+	'air-temperature': { units: '°C' },
+	'water-temperature': { units: '°C' },
+	'water-level': { units: 'mm' },
+	pH: { units: '', verbose: 'pH' },
+	'average-illuminance-at-netcup': { units: 'lux' },
+	co2: { units: 'ppm' }
+};
+
+export function verboseConditionName(conditionName: keyof ConditionsMeasurements) {
+	return getConditionMetadata(conditionName).verbose || conditionName.replaceAll('-', ' ');
+}
