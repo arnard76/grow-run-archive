43d7e92fe3755d583e7faa528082adf9f4af67c2
Arnav <arnard76@gmail.com>
2025-04-22 20:50:23 +1200

    update definitions

--- packages/definitions/src/datetime.ts
+++ packages/definitions/src/datetime.ts
@@ -1,1 +1,8 @@
 export type DateTime = string;
+
+export const displayFormatForDateTime = (date: string, timeZone?: string) =>
+	new Intl.DateTimeFormat('en-NZ', {
+		dateStyle: 'medium',
+		timeStyle: 'short',
+		timeZone
+	}).format(new Date(date));
--- packages/definitions/src/index.ts
+++ packages/definitions/src/index.ts
@@ -5,6 +5,7 @@ export * from './condition';
 export * from './duration';
 export * from './location';
 export * from './resource';
+export * from './datetime';
 
 // should I expose general entities from here?
 export * from './entity/actions';
