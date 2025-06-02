788d935d180eb224305edb959f26ddc6dd484796
Arnav <arnard76@gmail.com>
2025-06-02 12:25:40 +1200

    seperate package for dayjs with plugins

--- e2e-tests/cypress/mocks/fastForward.ts
+++ e2e-tests/cypress/mocks/fastForward.ts
@@ -1,11 +1,11 @@
-import dayjs, { ManipulateType } from 'dayjs';
+import dayjs from 'dayjs';
 export let fastForwardedTime = dayjs();
 
 /**
  * This only fast forwards the date and time on the user's computer
  * IT DOES NOT fast forward the actual time! (so other computers are still using the actual time)
  */
-export function fastForward(num: number, duration: ManipulateType = 'days') {
+export function fastForward(num: number, duration: dayjs.ManipulateType = 'days') {
 	cy.clock().invoke('restore');
 	fastForwardedTime = fastForwardedTime.add(num, duration);
 	cy.clock(new Date(fastForwardedTime.valueOf()), ['Date']);
