f2cbba1dd26da22a0eb4b5a880f6d6e0872725d5
Arnav <arnard76@gmail.com>
2025-07-07 23:05:59 +1200

    save location address (city & country) to database
    to avoid reverse geocoding when viewing data and also see location for all GRs

--- app/src/features/grow-runs/grow-run/details/location/Action.svelte
+++ app/src/features/grow-runs/grow-run/details/location/Action.svelte
@@ -1,13 +1,58 @@
 <script lang="ts">
+	import { PUBLIC_API_URL } from '$env/static/public';
 	import type GrowRun from '$features/grow-runs/grow-run';
-	import ActionTemplate from '$lib/components/ActionTemplate.svelte';
-	import { growRunActionNames } from '@grow-run-archive/definitions';
-	import Inputs from './Inputs.svelte';
+	import { parseAddress } from '$features/grow-runs/grow-run/details/location/geocode';
 	import { growRunsAPI } from '$features/grow-runs/grow-run/store';
+	import ActionTemplate from '$lib/components/ActionTemplate.svelte';
+	import { growRunActionNames, type Coords } from '@grow-run-archive/definitions';
+	import Inputs from './Inputs.svelte';
 
 	export let growRun: GrowRun;
 	export let closeModal: () => any;
+
+	let updatedGrowRunCoords: Coords | undefined = growRun.location && {
+		latitude: growRun.location.latitude,
+		longitude: growRun.location.longitude
+	};
+
+	const MAX_ADDRESS_UPDATE_INTERVAL = 5000; // in ms
+	let lastAddressUpdate: null | number = null;
+	let lastCoords: any = undefined;
+	let addressDisplayFormat: string | undefined;
 	let updatedGrowRunLocation: GrowRun['location'] = growRun.location;
+
+	$: updateLocationStuff(updatedGrowRunCoords);
+
+	async function updateLocationStuff(coordinates: Coords | undefined) {
+		if (!coordinates || !coordinates.longitude || !coordinates.latitude) {
+			addressDisplayFormat = undefined;
+			return;
+		}
+		if (lastCoords === coordinates) return;
+
+		if (lastAddressUpdate && Date.now() < lastAddressUpdate + MAX_ADDRESS_UPDATE_INTERVAL) {
+			setTimeout(() => updateLocationStuff(coordinates), MAX_ADDRESS_UPDATE_INTERVAL);
+			return;
+		}
+
+		lastAddressUpdate = Date.now();
+		lastCoords = { ...coordinates };
+
+		const geocodeResult = await (
+			await fetch(`${PUBLIC_API_URL}/grow-run/location`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ coords: coordinates })
+			})
+		).json();
+
+		try {
+			addressDisplayFormat = parseAddress(geocodeResult);
+			updatedGrowRunLocation = { ...coordinates, address: geocodeResult.address };
+		} catch (e) {
+			console.error("Couldn't parse city or address from " + JSON.stringify({ geocodeResult }));
+		}
+	}
 </script>
 
 <ActionTemplate
@@ -15,5 +60,9 @@
 	onComplete={() => growRunsAPI.updatePartial(growRun.id, { location: updatedGrowRunLocation })}
 	onCancel={closeModal}
 >
-	<Inputs bind:updatedGrowRunLocation />
+	<Inputs bind:updatedGrowRunCoords />
+
+	{#if addressDisplayFormat}
+		<p class="w-full text-wrap">Address: {addressDisplayFormat}</p>
+	{/if}
 </ActionTemplate>
