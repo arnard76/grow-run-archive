e36fcadc389f5c33aaaa65614235501bb18892d7
Arnav <arnard76@gmail.com>
2025-07-07 17:59:02 +1200

    use timezones library instead of maintaining timezones information

--- app/src/features/grow-runs/grow-run/conditions/TimezoneInput.svelte
+++ app/src/features/grow-runs/grow-run/conditions/TimezoneInput.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-	import timezones from './timezones.json';
-	const timezonesSorted = timezones.sort((tz1, tz2) => (tz1.offset < tz2.offset ? -1 : 1));
+	import timezones from 'timezones-list';
 
 	export let timezone: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
 </script>
 
-<label class="flex flex-wrap gap-2 items-center">
+<label class="flex flex-wrap gap-2 items-center text-wrap overflow-hidden break-words">
 	Timezone
 	<select bind:value={timezone}>
-		{#each timezonesSorted as timezone (timezone.text)}
-			{#each timezone.utc as value}
-				<option {value}>{timezone.offset} {value}</option>
-			{/each}
+		{#each timezones as timezone (timezone)}
+			<option value={timezone.tzCode}>{timezone.name}</option>
 		{/each}
 	</select>
 </label>
