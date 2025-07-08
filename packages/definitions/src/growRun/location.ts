e29c322ef62e91ce7d7ab841ecc8296341f319a0
Arnav <arnard76@gmail.com>
2025-07-08 19:56:25 +1200

    squash

--- packages/definitions/src/growRun/location.ts
+++ packages/definitions/src/growRun/location.ts
@@ -1,2 +1,3 @@
+// for nz, use county to represent the city because 'Auckland' is considered a county lol
 export type Location = Coords & { address: { city: string; county: string; country: string } };
 export type Coords = { longitude: number; latitude: number };
