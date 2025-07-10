441707edb913d7cf7303389a8eedfdeff330f992
Arnav <arnard76@gmail.com>
2025-07-10 23:06:28 +1200

    don't break build if optional environment variables are missing
    but TODO typechecking as part of #30

--- api/src/services/database/firebase/auth.ts
+++ api/src/services/database/firebase/auth.ts
@@ -1,7 +1,7 @@
 export async function getUser(username: string, password: string) {
 	const verifyUserApiUrl =
-		process.env.PUBLIC_ENV === 'test'
-			? `http://${process.env.PUBLIC_FIREBASE_AUTH_EMULATOR_HOST}/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.SECRET_FIREBASE_API_KEY}`
+		process.env.PUBLIC_ENV === 'test' && process.env.FIREBASE_AUTH_EMULATOR_HOST
+			? `http://${process.env.FIREBASE_AUTH_EMULATOR_HOST}/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.SECRET_FIREBASE_API_KEY}`
 			: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.SECRET_FIREBASE_API_KEY}`;
 	const verifyUserPayload = JSON.stringify({
 		email: username,
