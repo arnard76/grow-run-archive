ef66d9079e4e89853d5ec4d1a475eaf4d651a512
Arnav <arnard76@gmail.com>
2025-07-16 11:36:51 +1200

    fix small bugs

--- app/src/features/grow-setups/AddAction.svelte
+++ app/src/features/grow-setups/AddAction.svelte
@@ -2,58 +2,25 @@
 	import { growSetupActionNames, type GrowSetupType } from '@grow-run-archive/definitions';
 	import ActionTemplate from '../../lib/components/ActionTemplate.svelte';
 	import { growSetupsAPI, growSetups } from './store';
+	import Inputs from './grow-setup/Inputs.svelte';
 
 	export let closeModal: () => any;
 
-	let nicknameInput: string | undefined;
-	let linkInput: string | undefined;
-	let previousVersion: GrowSetupType['version'] | null = null;
+	let nickname: string | undefined;
+	let link: string | undefined;
+	let version: string;
 
-	$: version = calculateNextVersion(previousVersion);
-
-	function calculateNextVersion(previousVersion: GrowSetupType['version'] | null) {
-		if (!previousVersion) {
-			if (!$growSetups.length) return '1';
-
-			previousVersion = $growSetups
-				.sort((a, b) => a.version.localeCompare(b.version))
-				.at(-1)!.version;
-		} else {
-			if (!previousVersion.includes('.')) previousVersion += '.0';
-		}
-
-		const versionComponents = previousVersion.split('.');
-
-		versionComponents[versionComponents.length - 1] = (
-			parseInt(versionComponents.at(-1)!) + 1
-		).toString();
-
-		return versionComponents.join('.');
+	function addAndReset() {
+		growSetupsAPI.add({ nickname, link, version });
+		link = undefined;
+		nickname = undefined;
 	}
 </script>
 
 <ActionTemplate
 	actionName={growSetupActionNames.add}
 	onCancel={closeModal}
-	onComplete={() => growSetupsAPI.add({ nickname: nicknameInput, link: linkInput, version })}
+	onComplete={addAndReset}
 >
-	{#if $growSetups.length}
-		<div class="horizontal-input-group">
-			<label for="version-input">Related grow setup</label>
-			<select bind:value={previousVersion}>
-				{#each $growSetups as growSetup (growSetup.id)}
-					<option value={growSetup.version}>{growSetup.name}</option>
-				{/each}
-				<option value={null}>None</option>
-			</select>
-		</div>
-	{/if}
-	<p>Version #{version}</p>
-	<label class="horizontal-input-group">
-		Nickname <input type="text" placeholder="grow setup nickname" bind:value={nicknameInput} />
-	</label>
-	<label class="horizontal-input-group">
-		Link
-		<input type="text" placeholder="grow setup link" bind:value={linkInput} />
-	</label>
+	<Inputs bind:version bind:nickname bind:link />
 </ActionTemplate>
